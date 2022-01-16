020cd9918b497b935dd87b523d53c53d2c1ac61f
LordAvorio <gadingramjets@gmail.com>
2022-01-17 04:20:45 +0700

    Adding Store Pokemon

--- src/Pages/CollectionPage/CollectionPage.js
+++ src/Pages/CollectionPage/CollectionPage.js
@@ -1,15 +1,62 @@
-import React from 'react'
+import React from "react";
 
-import './CollectionPage.css'
+import Grid from "@mui/material/Grid";
+import Skeleton from "@mui/material/Skeleton";
+import Card from "@mui/material/Card";
 
-function CollectionPage() {
-    return (
-        <>
-            <div style={{marginTop: "20%"}}>
-                <p>Collection</p>
-            </div>
-        </>
-    )
+import "./CollectionPage.css";
+
+function CollectionPage(props) {
+
+    console.log(props.dataMyPokemon)
+
+  return (
+    <>
+      <div style={{ marginTop: "20%", padding: "0 5%"  }}>
+        <Grid container spacing={2}>
+          {props.dataMyPokemon ? (
+            props.dataMyPokemon.map((item, index) => {
+              return (
+                <Grid item xs={6} key={index}>
+                  <Card className="pokeCardLayout">
+                     <Grid container>
+                         <Grid item xs={12} className="insideLayoutCard">
+                            <p className="pokeNameTitle">{item.NickPokemon}</p>
+                         </Grid>
+                         <Grid item xs={12} className="insideLayoutCard">
+                            <img src={props.dataMyPokemon ? item.PokemonURL : ""} className='imagePokemon'/>
+                         </Grid>
+                         <Grid item xs={12} className="insideLayoutCard">
+                            <p className="pokeTypeTitle">{item.TypePokemon}</p>
+                         </Grid>
+                         <Grid item xs={12} className="insideLayoutCard">
+                            <button className="buttonTheme" onClick={() => props.releasePokemon(item.NickPokemon)}>Release</button>
+                         </Grid>
+                     </Grid> 
+                  </Card>
+                </Grid>
+              );
+            })
+          ) : (
+            <>
+              <Grid item xs={6}>
+                <Skeleton variant="rectangular" height={110} />
+              </Grid>
+              <Grid item xs={6}>
+                <Skeleton variant="rectangular" height={110} />
+              </Grid>
+              <Grid item xs={6}>
+                <Skeleton variant="rectangular" height={110} />
+              </Grid>
+              <Grid item xs={6}>
+                <Skeleton variant="rectangular" height={110} />
+              </Grid>
+            </>
+          )}
+        </Grid>
+      </div>
+    </>
+  );
 }
 
-export default CollectionPage
+export default CollectionPage;
--- src/Pages/DetailPage/DetailLogic.js
+++ src/Pages/DetailPage/DetailLogic.js
@@ -1,19 +1,93 @@
-import React from 'react'
-import {useLocation} from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { bake_cookie, read_cookie, delete_cookie } from 'sfcookies';
 
-import DetailPage from './DetailPage'
-
+import DetailPage from "./DetailPage";
 
 function DetailLogic() {
+  const [openModalInput, setOpenModalInput] = useState(false);
+  const [openModalFailed, setOpenModalFailed] = useState(false);
+  const [openModalSameNick, setOpenModalNick] = useState(false);
+  const [pokemonNick, setPokemonNick] = useState("");
 
-    const location = useLocation();
-    console.log(location.state.dataPokemon)
+  const location = useLocation();
+  console.log(location.state.dataPokemon);
 
-    return (
-        <>
-            <DetailPage dataPokemon={location.state.dataPokemon}/>
-        </>
-    )
+  const catchingPokemon = () => {
+    let randomNumber = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+    console.log(randomNumber);
+    if (randomNumber > 50) {
+      console.log("Success");
+      setOpenModalInput(true);
+    } else {
+      console.log("Failed");
+      setOpenModalFailed(true);
+    }
+  };
+
+  const closeModalFailed = () => {
+    setOpenModalFailed(false);
+  };
+
+  const closeModalSameNick = () => {
+    setOpenModalNick(false);
+    setOpenModalInput(false);
+    setPokemonNick("");
+  }
+
+  const storePokemon = () => {
+    let checkCookie = read_cookie('store_pokemon')  
+    let arrayPokemon = []  
+    let storePokemon = {}
+    let checkSame = false
+    console.log(checkCookie)
+
+    for(let x = 0; x < checkCookie.length; x++){
+      if(checkCookie[x].TypePokemon === location.state.dataPokemon.name && checkCookie[x].NickPokemon === pokemonNick){
+        checkSame = true
+      }
+    }
+
+    if(checkSame === true){
+      setOpenModalNick(true)
+      setOpenModalInput(false);
+      setPokemonNick("");
+    }else{
+      storePokemon["TypePokemon"] = location.state.dataPokemon.name;
+      storePokemon["NickPokemon"] = pokemonNick;
+      storePokemon["PokemonURL"] = location.state.dataPokemon.sprites.other.dream_world.front_default;
+      arrayPokemon.push(storePokemon)
+  
+      if(checkCookie){
+          checkCookie.push(storePokemon)
+          bake_cookie("store_pokemon", checkCookie)
+      }else{
+          bake_cookie("store_pokemon", arrayPokemon)       
+      }
+      console.log(read_cookie('store_pokemon'))
+    }
+
+
+    setOpenModalInput(false);
+    setPokemonNick("");
+  };
+
+  return (
+    <>
+      <DetailPage
+        dataPokemon={location.state.dataPokemon}
+        catchingPokemon={catchingPokemon}
+        openModalInput={openModalInput}
+        openModalFailed={openModalFailed}
+        closeModalFailed={closeModalFailed}
+        storePokemon={storePokemon}
+        pokemonNick={pokemonNick}
+        setPokemonNick={setPokemonNick}
+        openModalSameNick={openModalSameNick}
+        closeModalSameNick={closeModalSameNick}
+      />
+    </>
+  );
 }
 
-export default DetailLogic
+export default DetailLogic;
